ff2bb8d2341d8c9ad9d767d6ee9a0a86e1c65c56
Mathis <web@mathisvalence.com>
2025-05-27 16:15:08 +0200

    ajout des script gsap à index.html et création d'un script d'animation dans script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-Commande ECHO activ‚e.
+
+ 
+ 
+ 
+ 
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText)
+  // gsap code here!
+
+
+gsap.to(".titre", { duration: 2, x: 300 });
+
+
+ });
