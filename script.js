b135f11558f453d5518ed52de7f241995b5cc628
Mathis <web@mathisvalence.com>
2025-05-28 00:15:16 +0200

    ajout de scroll trigger pour le titre et le paragraphe

--- script.js
+++ script.js
@@ -4,18 +4,29 @@
 
  
  // use a script tag or an external JS file
+
+
+
  document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText)
-  // gsap code here!
+
+
+
+
+  // gsap code here
 
 
 
 const slideEase = "cubic-bezier(0.65,0.05,0.36,1)";
+
+
+
 let split = SplitText.create(".split", {
 
   type: "words",
   
   autoSplit: true,
+
   onSplit: (self) => {
     return gsap.from(self.words, {
       y: 70,
@@ -23,11 +34,25 @@ let split = SplitText.create(".split", {
       rotate:15,
       stagger: 0.06,
       ease: slideEase,
-      duration: 1,
-    
+      
+
+        scrollTrigger: {
+         scrub: 1,
+        trigger: ".split",
+        start: "top 80%",
+        end: "top 40%", // Quand le haut de l'élément atteint 80% de la fenêtre
+        toggleActions: "play none none reverse", // jouer à l'entrée, rien à la sortie
+         markers: true
+      }
+
     });
   }
 });
+
+
+
+
+
 let split2 = SplitText.create(".split2", {
 
  type: "words, lines", // only split into words and lines (not characters)
@@ -37,26 +62,26 @@ let split2 = SplitText.create(".split2", {
   autoSplit: true,
   onSplit: (self) => {
     return gsap.from(self.words, {
-
-
-
-
-
-
       y: 30,
       rotate:5,
       opacity: 0,
       stagger: 0.01,
-      
-      ease: slideEase,
-        
-      
-    });
+       ease: slideEase,
+
+
+               scrollTrigger: {
+         scrub: 1,
+        trigger: ".split2",
+         // Quand le haut de l'élément atteint 80% de la fenêtre
+        start: "top 80%",
+        end: "top 40%",
+        toggleActions: "play none none reverse", // jouer à l'entrée, rien à la sortie
+         markers: true
+      }
+
+        });
   }
 });
-
-
-
- });
+});
 
  
